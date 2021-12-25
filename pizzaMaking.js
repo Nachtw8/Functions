e6cc7ff2061eaf1301bcf70002f6ad227052daba
Nachtw8 <94112184+Nachtw8@users.noreply.github.com>
2021-12-25 12:31:19 +0100

    Add files via upload

---
+++ pizzaMaking.js
@@ -0,0 +1,25 @@
+console.log ('**** M E N U  ****');
+
+function pizzaMaking() {
+    console.log('**** MENU: How to Make Pizza ****');
+    console.log("Meel maken");
+    console.log("Groente voorbereiden");
+    console.log("In de oven zetten");
+    console.log("15 minuten in de overn houden");
+    console.log("Bedienen");
+}
+
+function howtoMakeSushi() {
+    console.log ('**** MENU: How to Make Sushi  ****');
+    console.log("Zoek op Internet het menu.");
+    console.log("Doe boodschappen");
+    console.log("Maak de sushi");
+    console.log("Bedienen en eten");
+}
+
+howtoMakeSushi();
+howtoMakeSushi();
+howtoMakeSushi();
+
+pizzaMaking();
+pizzaMaking();
